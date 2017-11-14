7a0d14781a8f5bb8be1cc7e0bce3c0943cb19436
Sebastian Kreuzer <sebastian.kreuzer@live.de>
2017-11-14 14:33:11 +0100

    views refacotored

--- src/HoloLensRezept/HoloLensRezept/Buylist.xaml.cs
+++ src/HoloLensRezept/HoloLensRezept/Buylist.xaml.cs
@@ -26,5 +26,10 @@ namespace HoloLensRezept
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Tools));
+        }
     }
 }
