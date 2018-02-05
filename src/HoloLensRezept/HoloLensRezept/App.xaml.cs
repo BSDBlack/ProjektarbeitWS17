357704330abaa9d454e7e0c9f9a4a4f13ba5995a
Sebastian Kreuzer <sebastian.kreuzer@live.de>
2018-02-05 15:29:52 +0100

    moved settings to application start

--- src/HoloLensRezept/HoloLensRezept/App.xaml.cs
+++ src/HoloLensRezept/HoloLensRezept/App.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace HoloLensRezept
@@ -130,7 +122,6 @@ namespace HoloLensRezept
             }
             catch (System.Exception e)
             {
-
             }
         }
     }
